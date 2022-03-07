358983d1ce1e8185be76556a29a9d533d37b5def
Anubhab Patnaik <anubhabr50@gmail.com>
2022-03-07 16:32:39 +0530

    Added the 2nd section of the landing page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -157,22 +157,90 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
+                const SizedBox(height: 5,),
                 Container(
                   height: 180,
                   width: MediaQuery.of(context).size.width,
                   child: Stack(
                     children: [
                       Container(
+                        margin: const EdgeInsets.only(top:30),
                         width: MediaQuery.of(context).size.width,
                         height: 120,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                            image: AssetImage(
+                          image: const DecorationImage(
+                            image:  AssetImage(
                               "assets/card.jpg"
+                            ),
+                            fit: BoxFit.fill
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 40,
+                              offset: Offset(8, 10),
+                              color: color.AppColor.gradientSecond.withOpacity(0.3),
+                            ),
+                            BoxShadow(
+                              blurRadius: 10,
+                              offset: Offset(-1, -5),
+                              color: color.AppColor.gradientSecond.withOpacity(0.3),
                             )
-                          )
+                          ]
                         ),
+                      ),
+                      Container(
+                        height: 200,
+                        width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.only(
+                            right: 200,
+                            bottom: 30,
+                            left: 25
+
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: const DecorationImage(
+                                  image:  AssetImage(
+                                      "assets/figure.png"
+                                  ),
+                                  //fit: BoxFit.fill
+                              ),
+
+                          )
+
+                      ),
+                      Container(
+                        width: double.maxFinite,
+                        height: 100,
+                        margin: const EdgeInsets.only(left: 150, top: 50),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "You're doing great!",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: color.AppColor.homePageDetail
+                              ),
+                            ),
+                            const SizedBox(height: 10,),
+                            RichText(text: TextSpan(
+                              text: "Keep it up \n",
+                              style: TextStyle(
+                                color: color.AppColor.homePagePlanColor
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: "Stick to your schedule",
+                                )
+                              ]
+                            ) )
+
+                          ],
+                        ),
+
                       )
                     ],
                   ),
